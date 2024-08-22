3366e0cb81e877357c6ef559afbca858bc4bc7f8
shay shalom izhak <alonizhakalonizhak@gmail.com>
2024-08-22 10:57:24 +0300

    added new changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './css/Header.css'; 
+import { FaMoon, FaSun } from 'react-icons/fa'; // Make sure to install react-icons
 
 interface HeaderProps {
   title: string;
@@ -7,10 +8,19 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ title, subtitle }) => {
+  const [isLightMode, setIsLightMode] = useState(true);
+
+  const toggleMode = () => {
+    setIsLightMode(prevMode => !prevMode);
+  };
+
   return (
-    <header className="header">
-      <h1 className="header-title" style={{fontFamily: "Oswald, sans-serif"}}>{title}</h1>
-      <p className="header-subtitle" style={{fontFamily: "Playwrite AU NSW, cursive"}}>{subtitle}</p>
+    <header className={`header ${isLightMode ? 'light-mode' : 'dark-mode'}`}>
+      <h1 className="header-title" style={{ fontFamily: "Oswald, sans-serif" }}>{title}</h1>
+      <p className="header-subtitle" style={{ fontFamily: "Playwrite AU NSW, cursive" }}>{subtitle}</p>
+      <button className="mode-toggle" onClick={toggleMode}>
+        {isLightMode ? <FaMoon /> : <FaSun />}
+      </button>
     </header>
   );
 };
